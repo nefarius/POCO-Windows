55e365b8b542a19785a131bc3e6ed4b7dcdf9d6f
Benjamin Höglinger <nefarius@dhmx.at>
2017-07-19 22:28:33 +0200

    Added clone script Corrected paths

--- poco_build.cmd
+++ poco_build.cmd
@@ -1,9 +1,14 @@
 @echo off
+@setlocal
 
-set MYSELF=%~dp0
-set POCO=%MYSELF%\poco
+pushd "%~dp0"
 
-cd "%POCO%"
+IF NOT EXIST "poco" (
+    echo "poco folder not found, did you forget to clone?"
+    exit /B -1
+)
+
+cd poco
 
 IF NOT EXIST "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
 	echo "Microsoft Visual Studio 2015 not found in default installation dir, can't continue"
@@ -21,3 +26,7 @@ rem Build x86
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 call buildwin 140 build shared both Win32 nosamples
 call buildwin 140 build static_mt both Win32 nosamples
+
+popd
+endlocal
+pause
---
+++ poco_clone.cmd
@@ -0,0 +1,11 @@
+@echo off
+pushd "%~dp0"
+
+echo "Cloning repository..."
+
+call git clone -b poco-1.7.8 https://github.com/pocoproject/poco.git
+
+echo "Cloning finished!"
+
+popd
+pause
