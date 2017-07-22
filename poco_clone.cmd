8922288e4687d58527c034ef1c1d8fd3d3909f8c
Benjamin Höglinger <nefarius@dhmx.at>
2017-07-22 10:21:05 +0200

    Added NuGet client download to clone script

--- poco_clone.cmd
+++ poco_clone.cmd
@@ -2,10 +2,12 @@
 pushd "%~dp0"
 
 echo Cloning repository...
-
 call git clone -b poco-1.7.8 https://github.com/pocoproject/poco.git
-
 echo Cloning finished!
+
+echo Fetching latest nuget client binary...
+call powershell -Command Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -OutFile "nuget.exe"
+echo Nuget downloaded!
 
 popd
 pause
