8b8a32fdcafe265e3b789122d3e13710d4b91958
Benjamin Höglinger <nefarius@dhmx.at>
2018-02-05 00:12:43 +0100

    Added Data/SQLite component

--- PrepareNuspec.psm1
+++ PrepareNuspec.psm1
@@ -1,18 +1,19 @@
 function Get-IncludeFiles()
 {
     $components = Get-Content .\components.txt
-    return (Get-ChildItem -Recurse .\poco\ | Where-Object { 
-        $component = [regex]"poco\\([a-zA-Z]*)\\include\\.*\.h$"
-        $match = $component.Match($_.FullName)
+    return (Get-ChildItem -Path .\poco\ -Filter *.h -Recurse -File -Name | Where-Object { 
+        $component = [regex]"([a-zA-Z0-9\\]*)\\include\\Poco\\.*\.h"
+        $match = $component.Match($_)
+        $componentName = $match.Groups[1].Value -replace "\\", "/"        
         
-        return ($match.Success -and $components.Contains($match.Groups[1].Value))
-    } | Select-Object @{ Name = "FullName"; Expression = { $_.FullName.Replace(("{0}\" -f (Convert-Path .)), "") } })
+        return ($match.Success -and $components.Contains($componentName))
+    } | Select-Object @{ Name = "FullName"; Expression = { $_.Replace(("{0}\" -f (Convert-Path .)), "") } })
 }
 
 function Get-XmlNodes()
 {
-    $sub = [regex]"poco\\[a-zA-Z]*\\(include\\.*\.h$)"
-    $template = "<file src=`"{0}`" target=`"lib\native\{1}`" />"
+    $sub = [regex]"\\(include\\.*\.h$)"
+    $template = "<file src=`"poco\{0}`" target=`"lib\native\{1}`" />"
    
     Get-IncludeFiles | ForEach-Object { $template -f $_.FullName, $sub.Match($_.FullName).Groups[1].Value }
 }
